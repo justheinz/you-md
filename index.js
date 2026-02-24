64eef06ced0035c3e71ad8f63a8fe178ca310740
justheinz <heinztech52@gmail.com>
2026-02-23 21:27:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
  * Creator: popkid
  * Improvements: auto-reconnect, error guards, keep-alive, safe plugins
  */
-if (global.started) return;
-global.started = true;
 
 console.clear()
 console.log("📳 Starting YOU-MD...")
